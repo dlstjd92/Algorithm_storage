3b7bf3ba6666a206b6ebca0c2101cae393078862
dlstjd92 <dlstjd222@gmail.com>
2024-08-28 09:01:39 +0000

    Your commit message here

---
+++ pintos-kaist-897754/threads/복잡도는 지수적으로 증가.c
@@ -0,0 +1,7 @@
+복잡도는 지수적으로 증가
+-> 를 해결하기 위해 분할정복 개념의 등장 -> 운영체제를 쪼개자 -> 리소스를 기준으로 쪼개자
+
+Decomposition & Abstraction 의 개념 탑 다운 뷰의 개념
+
+1. abstractions 2. protection & Isolation 3. sharing 
+Os의 중요한개념
